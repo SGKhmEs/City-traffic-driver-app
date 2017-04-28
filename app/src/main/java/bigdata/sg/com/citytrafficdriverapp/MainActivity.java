eccce53bbc1d54c157ba1f3a7752e03dd3f32cec
bonvit <bonarv96@gmail.com>
2017-04-28 22:57:09 +0300

    Init project commit

---
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/MainActivity.java
@@ -0,0 +1,13 @@
+package bigdata.sg.com.citytrafficdriverapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
