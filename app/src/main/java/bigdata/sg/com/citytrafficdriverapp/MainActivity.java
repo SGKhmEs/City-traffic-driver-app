5f7633fe3cd23f20fea5364be7c327eab9d0bffa
bonvit <bonarv96@gmail.com>
2017-05-09 21:34:06 +0300

    Designed MainActivity with presenter. Added checking and requesting permissions. Added button that starts/stops services.

--- app/src/main/java/bigdata/sg/com/citytrafficdriverapp/MainActivity.java
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/MainActivity.java
@@ -1,42 +1,49 @@
 package bigdata.sg.com.citytrafficdriverapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-import bigdata.sg.com.citytrafficdriverapp.Services.QrScanService;
-import bigdata.sg.com.citytrafficdriverapp.Services.ServiceGPS;
+import bigdata.sg.com.citytrafficdriverapp.Presenters.MainPresenter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button mButtonStart;
-    private Button mButtonStop;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
+    @BindView(R.id.mainActivityButtonControl)
+    Button mButtonControl;
+
+    MainPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        mButtonStart = (Button) findViewById(R.id.btn_start);
-        mButtonStart.setOnClickListener(this);
-        mButtonStop = (Button) findViewById(R.id.btn_stop) ;
-        mButtonStop.setOnClickListener(this);
+        ButterKnife.bind(this);
 
+        mPresenter = new MainPresenter(this);
+        mPresenter.requestPermissions();
+        mPresenter.validate();
 
-        startService(ServiceGPS.newIntent(this));
-        QrScanService.setServiceAlarm(this, true);
+        mButtonControl.setOnClickListener(mPresenter);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btn_start:
-                QrScanService.setServiceAlarm(this, true);
-                break;
-            case R.id.btn_stop:
-                QrScanService.setServiceAlarm(this, false);
-                break;
+    public void changeButtonState(boolean state)
+    {
+        if(state)
+        {
+            //if already running
+            mButtonControl.setText(R.string.Stop);
+        }
+        else
+        {
+            //if stopped
+            mButtonControl.setText(R.string.Start);
         }
     }
+
+
+
 }
