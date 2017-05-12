c98739e09c39bf6222801adb300ea6a6d596c724
bonvit <bonarv96@gmail.com>
2017-05-12 17:31:13 +0300

    Class that allows putting data to database or sending to the rest server (TODO).

---
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/DBRestWriter.java
@@ -0,0 +1,36 @@
+package bigdata.sg.com.citytrafficdriverapp;
+
+import android.content.Context;
+
+import bigdata.sg.com.citytrafficdriverapp.database.DaoDatabase;
+import bigdata.sg.com.citytrafficdriverapp.database.Entities.AuthData;
+import bigdata.sg.com.citytrafficdriverapp.database.Entities.GpsData;
+import bigdata.sg.com.citytrafficdriverapp.database.IDatabase;
+
+/**
+ * Created by bonar on 5/12/2017.
+ */
+
+public class DBRestWriter implements DataWriter {
+    private static final String TAG = "DBRestWriter";
+
+    private IDatabase mDatabase;
+
+    public DBRestWriter(Context context) {
+        mDatabase = new DaoDatabase(context);
+    }
+
+    @Override
+    public void write(GpsData data) {
+        mDatabase.insert(data);
+
+        //TODO Should post data on server or to database if internet is unavailable
+    }
+
+    @Override
+    public void write(AuthData data) {
+        mDatabase.insert(data);
+
+        //TODO Should post data on server or to database if internet is unavailable
+    }
+}
