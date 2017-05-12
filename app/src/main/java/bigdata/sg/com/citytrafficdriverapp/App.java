cfe619037c5be167ae7ffafd06c62cb714a3a03d
bonvit <bonarv96@gmail.com>
2017-05-12 17:32:32 +0300

    Now provides DataWriter instead of DaoSession.

--- app/src/main/java/bigdata/sg/com/citytrafficdriverapp/App.java
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/App.java
@@ -2,29 +2,21 @@ package bigdata.sg.com.citytrafficdriverapp;
 
 import android.app.Application;
 
-import org.greenrobot.greendao.database.Database;
-
-import bigdata.sg.com.citytrafficdriverapp.database.DaoMaster;
-import bigdata.sg.com.citytrafficdriverapp.database.DaoMaster.DevOpenHelper;
-import bigdata.sg.com.citytrafficdriverapp.database.DaoSession;
-
 public class App extends Application {
     private static final String TAG = "App";
 
-    private static final String DB_NAME = "city-traffic-data-db";
-
-    private DaoSession daoSession;
+    private DataWriter mDataWriter;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        DevOpenHelper helper = new DevOpenHelper(this, DB_NAME);
-        Database db = helper.getWritableDb();
-        daoSession = new DaoMaster(db).newSession();
+        mDataWriter = new DBRestWriter(this);
     }
 
-    public DaoSession getDaoSession() {
-        return daoSession;
+    public DataWriter getDataWriter()
+    {
+        return mDataWriter;
     }
+
 }
