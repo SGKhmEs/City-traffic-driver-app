5096437e28e37179db474aca8e67df27790f1ea3
bonvit <bonarv96@gmail.com>
2017-05-18 16:25:09 +0300

    Added feature: saves data via DataWriter.

--- app/src/main/java/bigdata/sg/com/citytrafficdriverapp/Services/ServiceGPS.java
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/Services/ServiceGPS.java
@@ -10,8 +10,13 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 
+import bigdata.sg.com.citytrafficdriverapp.App;
+import bigdata.sg.com.citytrafficdriverapp.Config;
+import bigdata.sg.com.citytrafficdriverapp.DataWriter;
 import bigdata.sg.com.citytrafficdriverapp.Services.Models.Client;
 import bigdata.sg.com.citytrafficdriverapp.Services.Models.GoogleGPSClient;
+import bigdata.sg.com.citytrafficdriverapp.Utils.DateProvider;
+import bigdata.sg.com.citytrafficdriverapp.database.Entities.GpsData;
 
 public class ServiceGPS extends Service {
     private static final String TAG = "ServiceGPS";
@@ -21,6 +26,8 @@ public class ServiceGPS extends Service {
      */
     private Client mClient;
 
+    private DataWriter mDataWriter;
+
     public static Intent newIntent(Context context)
     {
         return new Intent(context, ServiceGPS.class);
@@ -29,6 +36,8 @@ public class ServiceGPS extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
+
+        mDataWriter = ((App) getApplication()).getDataWriter();
 
         mClient = new GoogleGPSClient(ServiceGPS.this, new GoogleGPSClient.Callbacks() {
             @Override
@@ -45,6 +54,11 @@ public class ServiceGPS extends Service {
             public void onLocationChanged(Location location) {
                 Log.d(TAG, "onLocationChanged: [" + location.getLatitude() + "; " + location.getLongitude() + "]"
                     + " | Speed: " + location.getSpeed() + " m/s | Accuracy: " + location.getAccuracy() + " m");
+
+                String currentDate = DateProvider.getCurrentDate(Config.DATE_FORMAT);
+                GpsData gpsData = new GpsData(location, currentDate, null);
+
+                mDataWriter.write(gpsData);
             }
         });
 
