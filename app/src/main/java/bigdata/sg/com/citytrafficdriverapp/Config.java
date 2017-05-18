9b4661d069e411228afd98a764ac590ba6169e46
Demandr <deman13108>
2017-05-18 18:54:56 +0300

    update config

--- app/src/main/java/bigdata/sg/com/citytrafficdriverapp/Config.java
+++ app/src/main/java/bigdata/sg/com/citytrafficdriverapp/Config.java
@@ -2,12 +2,10 @@ package bigdata.sg.com.citytrafficdriverapp;
 
 import bigdata.sg.com.citytrafficdriverapp.Services.Helpers.ServiceAlarmManager;
 
-/**
- * Created by bonar on 5/18/2017.
- */
 
 public class Config {
-    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
+
 
     public static final long SERVICEGPS_WAKEUP_INTERVAL = ServiceAlarmManager.MINS_30;
     public static final long AUTH_INTERVAL = ServiceAlarmManager.HOURS_2;
